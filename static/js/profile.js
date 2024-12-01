dbfa34e653668cf6bcea19339d2442742139fd85
WANG <mr233why@gmail.com>
2024-12-01 15:11:40 +0800

    profile.js with some problem

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,3 +1,4 @@
+//Yeung Chin To 22084296D, WANG Haoyu 22102084D
 document.getElementById('logoutButton').addEventListener('click', async () => {
     try {
         const response = await fetch('/auth/logout', {
@@ -15,4 +16,30 @@ document.getElementById('logoutButton').addEventListener('click', async () => {
         alert("Error during logout: " + error.message);
         console.error('Logout error:', error);
     }
+});
+
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const response = await fetch('/auth/me', {
+            method: 'GET',
+            credentials: 'include'
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch user details');
+        }
+
+        const data = await response.json();
+
+        if (data.status === "success") {
+            document.getElementById('name').value = data.user.nickname;
+            document.getElementById('email').value = data.user.email;
+        } else {
+            alert(data.message);
+        }
+    } catch (error) {
+        alert("Error fetching user details: " + error.message);
+        console.error('Fetch error:', error);
+    }
+
 });
